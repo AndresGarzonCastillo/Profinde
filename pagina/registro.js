470175ba41c526068ccca91feb94fe0eba5a3077
Andres Garzon <115560952+AndresGarzonCastillo@users.noreply.github.com>
2023-07-03 14:11:14 -0500

    Add files via upload

--- pagina/registro.js
+++ pagina/registro.js
@@ -1,171 +1,115 @@
-async function validarFormulario() {
-    var nombres = document.forms["registro1"]["nombres"].value;
-    var apellidos = document.forms["registro1"]["apellidos"].value;
-    var email = document.forms["registro1"]["email"].value;
-    var celular = document.forms["registro1"]["celular"].value;
-    var password = document.forms["registro1"]["password"].value;
-
-    var tipoDoc = document.forms["registro2"]["tipoDoc"].value;
-    var numDoc = document.forms["registro2"]["numDoc"].value;
-    var direccion = document.forms["registro2"]["direccion"].value;
-    var ciudad = document.forms["registro2"]["ciudad"].value;
-    var fecha = document.forms["registro2"]["fecha"].value;
-
-    var foto1 = document.forms["registro3"]["foto1"].value;
-    var foto2 = document.forms["registro3"]["foto2"].value;
-    var foto3 = document.forms["registro3"]["foto3"].value;
-    var foto4 = document.forms["registro3"]["foto4"].value;
-    var checkbox = document.getElementById("termsCheckbox").checked;
-
-    var errorNombres = document.getElementById("errorNombres");
-    var errorApellidos = document.getElementById("errorApellidos");
-    var errorEmail = document.getElementById("errorEmail");
-    var errorCelular = document.getElementById("errorCelular");
-    var errorPassword = document.getElementById("errorPassword");
-    var errorTipoDoc = document.getElementById("errorTipoDoc");
-    var errorDocumento = document.getElementById("errorDocumento");
-    var errorDireccion = document.getElementById("errorDireccion");
-    var errorCiudad = document.getElementById("errorCiudad");
-
-    var errores = [];
-
-    function mostrarError(elemento, mensaje) {
-        elemento.innerHTML = mensaje;
-        elemento.style.display = "block";
-    }
-
-    function ocultarError(elemento) {
-        elemento.innerHTML = "";
-        elemento.style.display = "none";
-    }
-
+function validarFormulario() {
+    // Obtener valores de los campos
+    var nombres = document.getElementById("nombres").value;
+    var apellidos = document.getElementById("apellidos").value;
+    var email = document.getElementById("email").value;
+    var celular = document.getElementById("celular").value;
+    var password = document.getElementById("password").value;
+    var tipoDoc = document.getElementById("tipoDoc").value;
+    var numDoc = document.getElementById("numDoc").value;
+    var direccion = document.getElementById("direccion").value;
+    var ciudad = document.getElementById("ciudad").value;
+    var fechaNacimiento = document.getElementById("fecha_nacimiento").value;
+    var foto1 = document.getElementById("foto1").value;
+    var foto2 = document.getElementById("foto2").value;
+    var foto3 = document.getElementById("foto3").value;
+    var foto4 = document.getElementById("foto4").value;
+    var terminos = document.getElementById("termsCheckbox").checked;
+    
+    // Validar campos
     if (nombres === "") {
-        errores.push("nombres");
-        mostrarError(errorNombres, "No ha ingresado sus nombres");
-    } else {
-        ocultarError(errorNombres);
+      alert("El campo Nombres es obligatorio.");
+      return false;
     }
-
+    
     if (apellidos === "") {
-        errores.push("apellidos");
-        mostrarError(errorApellidos, "No ha ingresado sus apellidos");
-    } else {
-        ocultarError(errorApellidos);
+      alert("El campo Apellidos es obligatorio.");
+      return false;
     }
-
+    
     if (email === "") {
-        errores.push("email");
-        mostrarError(errorEmail, "No ha ingresado un correo electrónico válido");
+      alert("El campo Email es obligatorio.");
+      return false;
     } else {
-        ocultarError(errorEmail);
+      // Validar estructura de email usando una expresión regular
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        alert("El campo Email no tiene un formato válido.");
+        return false;
+      }
     }
-
+    
     if (celular === "") {
-        mostrarError(errorCelular, "Por favor, ingresa tu número de celular.");
-        return false;
-    } else if (celular.length !== 10) {
-        mostrarError(errorCelular, "El número de celular debe tener exactamente 10 dígitos.");
-        return false;
-    } else {
-        ocultarError(errorCelular);
+      alert("El campo Número de celular es obligatorio.");
+      return false;
+    } else if (isNaN(celular) || celular.length !== 10) {
+      alert("El campo Número de celular debe tener 10 dígitos.");
+      return false;
     }
-
+    
     if (password === "") {
-        mostrarError(errorPassword, "Por favor, ingresa tu contraseña.");
-        return false;
-    } else if (!/[A-Z]/.test(password)) {
-        mostrarError(errorPassword, "La contraseña debe contener al menos una letra mayúscula.");
-        return false;
-    } else if (!/[0-9]/.test(password)) {
-        mostrarError(errorPassword, "La contraseña debe contener al menos un número.");
-        return false;
-    } else if (password.length < 8) {
-        mostrarError(errorPassword, "La contraseña debe tener al menos 8 caracteres.");
-        return false;
+      alert("El campo Contraseña es obligatorio.");
+      return false;
     } else {
-        ocultarError(errorPassword);
+      // Validar estructura de contraseña usando una expresión regular
+      var passwordRegex = /^(?=.*\d)(?=.*[A-Z]).{8,}$/;
+      if (!passwordRegex.test(password)) {
+        alert("El campo Contraseña no cumple con los requisitos mínimos.");
+        return false;
+      }
     }
-
+    
     if (tipoDoc === "") {
-        errores.push("tipoDoc");
-        mostrarError(errorTipoDoc, "No ha elegido un tipo de documento");
-    } else {
-        ocultarError(errorTipoDoc);
-    }
+        alert("Debe seleccionar Tipo de Documento.");
+        return false;
+      }
 
-    if (numDoc === "") {
-        errores.push("numDoc");
-        mostrarError(errorDocumento, "No ha ingresado su número de documento");
-    } else {
-        ocultarError(errorDocumento);
-    }
+      if (numDoc === "") {
+        alert("El campo Numero de Documento es obligatorio.");
+        return false;
+      }
 
-    if (direccion === "") {
-        errores.push("direccion");
-        mostrarError(errorDireccion, "No ha ingresado su dirección");
-    } else {
-        ocultarError(errorDireccion);
-    }
+      if (direccion === "") {
+        alert("El campo Direccion es obligatorio.");
+        return false;
+      }
 
     if (ciudad === "") {
-        errores.push("ciudad");
-        mostrarError(errorCiudad, "No ha elegido una ciudad");
+      alert("Debe seleccionar una Ciudad.");
+      return false;
+    }
+    
+    if (fechaNacimiento === "") {
+      alert("El campo Fecha de Nacimiento es obligatorio.");
+      return false;
     } else {
-        ocultarError(errorCiudad);
+      // Obtener fecha actual
+      var fechaActual = new Date();
+      // Obtener fecha de nacimiento ingresada
+      var fechaNacimientoDate = new Date(fechaNacimiento);
+      // Calcular la diferencia en milisegundos
+      var diferencia = fechaActual - fechaNacimientoDate;
+      // Convertir la diferencia a años
+      var edad = Math.floor(diferencia / (1000 * 60 * 60 * 24 * 365.25));
+      // Validar si es mayor de 18 años
+      if (edad < 18) {
+        alert("Debe ser mayor de 18 años para registrarse.");
+        return false;
+      }
+    }
+    
+    if (foto1 === "" && foto2 === "" && foto3 === "" && foto4 === "") {
+        alert("Debe adjuntar las fotos requeridas.");
+        return false;
     }
 
-    if (foto1 === "" || foto2 === "" || foto3 === "" || foto4 === "") {
-        errores.push("fotos");
-        console.log("No ha subido todas las fotos requeridas");
+    if (!terminos) {
+      alert("Debe aceptar los términos y condiciones.");
+      return false;
     }
-
-    if (!checkbox) {
-        errores.push("terms");
-        console.log("Debe aceptar términos y condiciones");
-    }
-
-    if (errores.length === 0) {
-        alert("Usuario en proceso de validación");
-        // Redireccionar a la página de inicio de sesión
-    } else {
-        for (var i = 0; i < errores.length; i++) {
-            switch (errores[i]) {
-                case "nombres":
-                    console.log("No ha ingresado sus nombres");
-                    break;
-                case "apellidos":
-                    console.log("No ha ingresado sus apellidos");
-                    break;
-                case "email":
-                    console.log("No ha ingresado un correo electrónico válido");
-                    break;
-                case "celular":
-                    console.log("No ha ingresado un número de celular válido");
-                    break;
-                case "password":
-                    console.log("No ha ingresado una contraseña válida");
-                    break;
-                case "tipoDoc":
-                    console.log("No ha elegido un tipo de documento");
-                    break;
-                case "numDoc":
-                    console.log("No ha ingresado su número de documento");
-                    break;
-                case "direccion":
-                    console.log("No ha ingresado su dirección");
-                    break;
-                case "ciudad":
-                    console.log("No ha elegido una ciudad");
-                    break;
-                case "fotos":
-                    console.log("No ha subido ninguna foto aún");
-                    break;
-                case "terms":
-                    console.log("Debe aceptar los términos y condiciones");
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-}
+    
+    // Si todos los campos son válidos, se envía el formulario
+    alert("Se han enviado los datos para validación.");
+    window.location.href = "inicioSesion.html";
+    return false;
+  }
